c9f6a02ebd20dfe5c3e7f5ee476a23b0798353a6
Carlos Roncero <croncero@icloud.com>
2025-12-18 18:12:13 +0100

    modificacion package.json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { configDefaults } from 'vitest/config'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',   // necesario para testear React
-    setupFiles: './src/setupTests.js',  // opcional, pero recomendado
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+    css: true,
+    // CAMBIO AQUI: Quitamos 'src/tests/' y ponemos '**' al principio.
+    // Esto asegura que coincida siempre, independientemente de la ruta base.
+    exclude: [...configDefaults.exclude, '**/e2e/**'],
   },
 })
 
